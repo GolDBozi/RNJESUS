90962ae193e9ed54cab9e927c65cf6b09f82fd4f
GolDBozi <xxxdjorkaeffxxx@gmail.com>
2017-03-06 21:18:25 +0200

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(91, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RNG1";
+            this.label1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(184, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RNG2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.Location = new System.Drawing.Point(268, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "RNG3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label4.Location = new System.Drawing.Point(360, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "RNG4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label5.Location = new System.Drawing.Point(445, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "RNG5";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label6.Location = new System.Drawing.Point(417, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 23);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "SUM1";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label7.Location = new System.Drawing.Point(417, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 23);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "SUM2";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label8.Location = new System.Drawing.Point(417, 225);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 23);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "SUM3";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label9.Location = new System.Drawing.Point(417, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 23);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "SUM4";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label10.Location = new System.Drawing.Point(417, 306);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 23);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "SUM5";
+            this.label10.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 5000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 5000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 5000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.Location = new System.Drawing.Point(26, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 95);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Time To Play The Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 42);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Show numbers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 42);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Show numbers";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(249, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 42);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Show numbers";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(249, 206);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 42);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Show numbers";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button6.Location = new System.Drawing.Point(26, 191);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(163, 95);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "If you want to play again restart the program!";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 375);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RNJESUS BE WITH YOU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//                                      Disclaimer
+
+ 
+//Den mou aresei katholou etc opws to exw kanei.
+//Einai leitourgiko kai logika kanei auto pou zhtate alla den maresei.
+//Mexri th kuriakh an exw xrono kai empneush tha prospathhsw na to kanw kalutero(programmatistika)
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {//edw kanoume thn class 
+        public class RNJesus
+        {//edw ftiaxnoume method pou epistrefei to tuxaio noumero.to 1000 einai apla ena orio.
+            Random rnd =new Random();
+            public int RNG()
+
+            {return rnd.Next(0,1000);}
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {//ftiaxnoume to object kai 2 pinakes
+            RNJesus a = new RNJesus();
+            int[] sum = new int[5];
+            int[] RNG = new int[5];
+            //ftiaxnoume ta 5 random numera kai ta vazoume se pinaka
+            RNG[0]= a.RNG();
+            RNG[1] = a.RNG();
+            RNG[2] = a.RNG();
+            RNG[3] = a.RNG();
+            RNG[4] = a.RNG();
+            //vazoume ta athroismata se allo pinaka
+            sum[0] = RNG[0];
+            sum[1] = RNG[0] + RNG[1];
+            sum[2] = RNG[0] + RNG[1] + RNG[2];
+            sum[3] = RNG[0] + RNG[1] + RNG[2] + RNG[3];
+            sum[4] = RNG[0] + RNG[1] + RNG[2] + RNG[3] + RNG[4];
+           
+            //thetoume tis times sta lables apo prin kai meta apla ta emfanizoume
+            label1.Text = Convert.ToString(RNG[0]);
+            label2.Text = Convert.ToString(RNG[1]);
+            label3.Text = Convert.ToString(RNG[2]);
+            label4.Text = Convert.ToString(RNG[3]);
+            label5.Text = Convert.ToString(RNG[4]);
+            label6.Text = Convert.ToString(sum[0]);
+            label7.Text = Convert.ToString(sum[1]);
+            label8.Text = Convert.ToString(sum[2]);
+            label9.Text = Convert.ToString(sum[3]);
+            label10.Text = Convert.ToString(sum[4]);
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {   //otan kanoume diplo click elegxoume pio label einai energo ekeinh th stigmh 
+            //gia na emfanisoume to antistoixo athroisma
+            //kai stamatame to epomeno timer
+            if ((label1.Visible) == true)
+                 {
+                timer2.Stop();
+                label6.Visible = true;
+                button6.Visible = true;
+                 }
+            else if ((label2.Visible) == true)
+                 {
+                timer3.Stop();
+                label7.Visible = true;
+                button2.Visible = true;
+                button6.Visible = true;
+                  }
+            else if ((label3.Visible) == true)
+                 {
+                timer4.Stop();
+                label8.Visible = true;
+                button3.Visible = true;
+                button6.Visible = true;
+                }
+            else if ((label4.Visible) == true)
+                {
+                timer5.Stop();
+                label9.Visible = true;
+                button4.Visible = true;
+                button6.Visible = true;
+                 }
+            else if ((label5.Visible) == true)
+                 {
+                label10.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                 }
+        }
+         
+        //to kathe timer perimenei 5 sec.Meta ksekinaei to epomeno timer kai emfanizei to current label
+        //kai stamataei to current timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {  
+            timer1.Interval = 5000;
+            timer2.Start();
+            label1.Visible = true;
+            timer1.Stop();
+        }
+        //sta epomena apla kruvei to prohgoumeno label gia na doulepsei to parapanw IF
+        private void timer2_Tick(object sender, EventArgs e)
+        { 
+            timer2.Interval = 5000;
+            label2.Visible = true;
+            label1.Visible = false;
+            timer3.Start();
+            timer2.Stop();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Interval = 5000;
+            timer4.Start();
+            label3.Visible = true;
+            label2.Visible = false;
+            timer3.Stop();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 5000;
+            timer5.Start();
+            label4.Visible = true;
+            label3.Visible = false;
+            timer4.Stop();
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        { 
+            timer5.Interval = 5000;
+           label5.Visible = true;
+            label4.Visible = false;
+            timer5.Stop();
+            //edw stamataei to teleuteo programma kai emfanizei to teliko athroisma
+            label10.Visible = true;
+            button6.Visible = true;
+            button5.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//edw ksekinaei h alusida.Mporousa na to kanw k alliws alla protimhsa auto
+            timer1.Start();
+            button1.Visible = false;
+        }
+        //edw ftiaxnw koumpi gia na emfanizei ta prohgoumena noumera gia epalhtheush
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+        }
+    }
+}
